8101b2e785bc9806a7d7dad265d981906337f3d5
Pankaj Khushalani <2182001pk@gmail.com>
2025-02-22 18:48:17 +0530

    feat: extract links from current tab
    Signed-off-by: Pankaj Khushalani <2182001pk@gmail.com>

--- content.js
+++ content.js
@@ -1,6 +1,21 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === "extractText") {
-    const text = document.body.innerText;
-    sendResponse({ content: text });
+    let links = [];
+    let matches = [];
+    const matchingWords = ["term", "policy", "privacy", "tos"];
+    const anchorTags = document.getElementsByTagName("a");
+    for (let i = 0; i < anchorTags.length; i++) {
+      links.push(anchorTags[i].href);
+    }
+    matchingWords.map((word) => {
+      links.map((link) => {
+        if (link.toLowerCase().includes(word)) {
+          matches.push(link);
+        }
+      });
+    });
+    sendResponse({
+      content: matches,
+    });
   }
 });
--- popup/popup.js
+++ popup/popup.js
@@ -1,11 +1,13 @@
-document.getElementById('summarize-btn').addEventListener('click', () => {
+document.getElementById("summarize-btn").addEventListener("click", () => {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    chrome.tabs.sendMessage(tabs[0].id, { action: "extractText" }, (response) => {
-      if (response && response.content) {
-        summarizeText(response.content).then(summary => {
-          document.getElementById('summary').innerText = summary;
-        });
+    chrome.tabs.sendMessage(
+      tabs[0].id,
+      { action: "extractText" },
+      (response) => {
+        if (response && response.content) {
+          document.getElementById("summary").innerText = response.content;
+        }
       }
-    });
+    );
   });
 });
