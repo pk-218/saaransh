e9caac3e6f7e3704588edcbfe2b565559caa406d
theshivv <shivamdarasingpawar@gmail.com>
2025-02-23 20:31:12 +0530

    merge fix

--- popup/popup.js
+++ popup/popup.js
@@ -1,3 +1,32 @@
+document.getElementById("analyze-btn").addEventListener("click", () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      chrome.tabs.sendMessage(tabs[0].id, { action: "extractAndSummarize" });
+  });
+
+  // Show content section and hide analyze button
+  document.getElementById("content-section").classList.remove("hidden");
+  document.getElementById("analyze-btn").classList.add("hidden");
+});
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === "getResult") {
+      console.log("Received result", request.result);
+      document.getElementById("summary").innerText = request.data;
+  }
+});
+
+// Toggle summary visibility
+document.getElementById("toggle-summary").addEventListener("click", () => {
+  const summarySection = document.getElementById("summary-section");
+  if (summarySection.classList.contains("hidden")) {
+      summarySection.classList.remove("hidden");
+      document.getElementById("toggle-summary").innerText = "Hide Summary";
+  } else {
+      summarySection.classList.add("hidden");
+      document.getElementById("toggle-summary").innerText = "Show Summary";
+  }
+});
+
 function sanitizeText(text) {
   const bodyStart = text.indexOf("<body");
   const bodyClose = text.indexOf("</body>", bodyStart);
@@ -38,15 +67,6 @@ function softmax(arr) {
   return expArr.map((val) => val / sumExpArr);
 }
 
-document.getElementById("summarize-btn").addEventListener("click", () => {
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    chrome.tabs.sendMessage(tabs[0].id, { action: "extractAndSummarize" });
-  });
-
-  // Show content section and hide analyze button
-  document.getElementById("content-section").classList.remove("hidden");
-  document.getElementById("analyze-btn").classList.add("hidden");
-});
 
 chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
   if (request.action === "getResult") {
@@ -55,14 +75,4 @@ chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
   }
 });
 
-// Toggle summary visibility
-document.getElementById("toggle-summary").addEventListener("click", () => {
-  const summarySection = document.getElementById("summary-section");
-  if (summarySection.classList.contains("hidden")) {
-    summarySection.classList.remove("hidden");
-    document.getElementById("toggle-summary").innerText = "Hide Summary";
-  } else {
-    summarySection.classList.add("hidden");
-    document.getElementById("toggle-summary").innerText = "Show Summary";
-  }
-});
+
